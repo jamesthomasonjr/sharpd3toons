997e278dc725adb81111e4c6592c4f165d56d8a7
James Thomason, Jr <james@jamesthomasonjr.com>
2014-03-30 17:18:12 -0400

    Simple routing

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php require 'vendor/autoload.php'; ?>
+<?php require 'vendor/autoload.php';
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+$app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/views'
+));
 
-</body>
-</html>
+$app->get('/', function() use($app) {
+	$subRequest = Symfony\Component\HttpFoundation\Request::create('/sharp-1324');
+	return $app->handle($subRequest, Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST, false);
+});
+
+$app->get('/{account}/', function($account) use($app) {
+	return $app['twig']->render('account.twig', array(
+		'account' => $account
+	));
+});
+
+$app->run();
+?>
