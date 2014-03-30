f71740b3ee51213b58e891906d9dd087686ed8f1
James Thomason, Jr <james@jamesthomasonjr.com>
2014-03-30 18:08:47 -0400

    Minimal clean up and logic added

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <?php require 'vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpKernel;
+use GuzzleHttp\Client;
+
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/views'
 ));
 
 $app->get('/', function() use($app) {
-	$subRequest = Symfony\Component\HttpFoundation\Request::create('/sharp-1324');
-	return $app->handle($subRequest, Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST, false);
+	$subRequest = Request::create('/sharp-1324/');
+	return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
 });
 
 $app->get('/{account}/', function($account) use($app) {
+
+	$client = new Client();
+
+	$response = $client->get('http://us.battle.net/api/d3/profile/'.$account.'/');
+	$profile = $response->json();
+
 	return $app['twig']->render('account.twig', array(
-		'account' => $account
+		'account' => $account,
+		'profile' => $profile
 	));
 });
 
