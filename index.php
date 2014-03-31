2527268ae7fbd1f05ef3bda3e411004b213d1e35
James Thomason, Jr <james@jamesthomasonjr.com>
2014-03-30 21:08:39 -0400

    Slight refactoring, more routing logic

--- index.php
+++ index.php
@@ -1,31 +1,81 @@
 <?php require 'vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation;
-use Symfony\Component\HttpKernel;
-use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
+$app['debug'] = true;
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/views'
 ));
 
-$app->get('/', function() use($app) {
-	$subRequest = Request::create('/sharp-1324/');
-	return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+$app['routes.controller'] = $app->share(function() use ($app) {
+	return new RouteController($app['request'], $app['twig']);
 });
 
-$app->get('/{account}/', function($account) use($app) {
+$app->get('/', 'routes.controller:index');
+$app->get('/{account}/', 'routes.controller:account');
 
-	$client = new Client();
+class RouteController
+{
+	protected $request;
+	protected $twig;
 
-	$response = $client->get('http://us.battle.net/api/d3/profile/'.$account.'/');
-	$profile = $response->json();
+	public function __construct(Request $request,Twig_Environment $twig)
+	{
+		$this->request = $request;
+		$this->twig = $twig;
+	}
 
-	return $app['twig']->render('account.twig', array(
-		'account' => $account,
-		'profile' => $profile
-	));
-});
+	public function index()
+	{
+		return $this->account('sharp-1324');
+	}
+
+	public function account($account)
+	{
+		$information = $this->grabAccountInformation($account);
+
+		return ($this->validateAccount($information))
+		? $this->displayAccount($information)
+		: $this->displayInvalidAccount() ;
+	}
+
+	public function validateAccount($information)
+	{
+		// If the result doesn't have a "battleTag" value then
+		// the account does not exist.
+		return (array_key_exists('battleTag', $information->json()));
+	}
+
+	public function grabAccountInformation($account)
+	{
+		$client = new GuzzleHttp\Client();
+
+		$response = $client->get('http://us.battle.net/api/d3/profile/'.$account.'/');
+		return $response;
+	}
+
+	public function displayAccount($information)
+	{
+		$profile = $information->json();
+
+		return $this->twig->render('account.twig', array(
+			'profile' => $profile
+		));
+	}
+
+	public function displayInvalidAccount()
+	{
+		return $this->twig->render('error.twig', array(
+			'error' => 'Invalid Account',
+			'description' => "The provided account does not exist."
+		));
+	}
+}
 
 $app->run();
+
 ?>
