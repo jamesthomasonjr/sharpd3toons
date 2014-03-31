457c73e9310d2116d7b19f511183661254f419e3
James Thomason, Jr <james@jamesthomasonjr.com>
2014-03-30 21:27:02 -0400

    Pulled RouteController out of index.php

---
+++ controllers/routes.php
@@ -0,0 +1,69 @@
+<?
+use Symfony\Component\HttpFoundation\Request;
+
+class RouteController
+{
+  protected $request;
+  protected $twig;
+
+  public function __construct(Request $request,Twig_Environment $twig)
+  {
+    $this->request = $request;
+    $this->twig = $twig;
+  }
+
+  public function index()
+  {
+    // Show my accout by default
+    return $this->account('sharp-1324');
+  }
+
+  public function account($account)
+  {
+    // Grab account data
+    $information = $this->grabAccountInformation($account);
+
+    // If account exists, show account page
+    // Otherwise, show an error page
+    return ($this->validateAccount($information))
+    ? $this->displayAccount($information)
+    : $this->displayInvalidAccount() ;
+  }
+
+  public function validateAccount($information)
+  {
+    // If the result doesn't have a "battleTag" value then
+    // the account does not exist.
+    return (array_key_exists('battleTag', $information->json()));
+  }
+
+  public function grabAccountInformation($account)
+  {
+    $client = new GuzzleHttp\Client();
+
+    // Grab Account profile
+    $response = $client->get('http://us.battle.net/api/d3/profile/'.$account.'/');
+    return $response;
+  }
+
+  public function displayAccount($information)
+  {
+    $profile = $information->json();
+
+    // Render the account information page
+    return $this->twig->render('account.twig', array(
+      'profile' => $profile
+    ));
+  }
+
+  public function displayInvalidAccount()
+  {
+    // Render an error page
+    return $this->twig->render('error.twig', array(
+      'error' => 'Invalid Account',
+      'description' => "The provided account does not exist."
+    ));
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?php require 'vendor/autoload.php';
+<?php
 
-use Symfony\Component\HttpFoundation\Request;
+require 'vendor/autoload.php';
+require 'controllers/routes.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -17,71 +18,6 @@ $app['routes.controller'] = $app->share(function() use ($app) {
 
 $app->get('/', 'routes.controller:index');
 $app->get('/{account}/', 'routes.controller:account');
-
-class RouteController
-{
-	protected $request;
-	protected $twig;
-
-	public function __construct(Request $request,Twig_Environment $twig)
-	{
-		$this->request = $request;
-		$this->twig = $twig;
-	}
-
-	public function index()
-	{
-		// Show my accout by default
-		return $this->account('sharp-1324');
-	}
-
-	public function account($account)
-	{
-		// Grab account data
-		$information = $this->grabAccountInformation($account);
-
-		// If account exists, show account page
-		// Otherwise, show an error page
-		return ($this->validateAccount($information))
-		? $this->displayAccount($information)
-		: $this->displayInvalidAccount() ;
-	}
-
-	public function validateAccount($information)
-	{
-		// If the result doesn't have a "battleTag" value then
-		// the account does not exist.
-		return (array_key_exists('battleTag', $information->json()));
-	}
-
-	public function grabAccountInformation($account)
-	{
-		$client = new GuzzleHttp\Client();
-
-		// Grab Account profile
-		$response = $client->get('http://us.battle.net/api/d3/profile/'.$account.'/');
-		return $response;
-	}
-
-	public function displayAccount($information)
-	{
-		$profile = $information->json();
-
-		// Render the account information page
-		return $this->twig->render('account.twig', array(
-			'profile' => $profile
-		));
-	}
-
-	public function displayInvalidAccount()
-	{
-		// Render an error page
-		return $this->twig->render('error.twig', array(
-			'error' => 'Invalid Account',
-			'description' => "The provided account does not exist."
-		));
-	}
-}
 
 $app->run();
 
